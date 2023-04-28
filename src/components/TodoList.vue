5858b3f0ee40391499db6f89a8dee4056e505ddf
taotao <1985504469@qq.com>
2023-04-28 22:35:30 +0800

    改成 script 标签 setup格式

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -15,47 +15,36 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import {ref} from 'vue'
 
-export default {
-  setup() {
-    // 初始化待办事项列表
-    const todos = ref([
-      {id: 1, title: 'Learn Vue 3', completed: false},
-      {id: 2, title: 'Build a Todo app', completed: true}
-    ])
+// 初始化待办事项列表
+const todos = ref([
+  {id: 1, title: 'Learn Vue 3', completed: false},
+  {id: 2, title: 'Build a Todo app', completed: true}
+])
 
-    // 初始化新待办事项
-    const newTodo = ref('')
+// 初始化新待办事项
+const newTodo = ref('')
 
-    // 添加新待办事项
-    const addTodo = () => {
-      if (newTodo.value.trim() !== '') {
-        todos.value.push({id: Date.now(), title: newTodo.value, completed: false})
-        newTodo.value = ''
-      }
-    }
-
-    // 更新待办事项完成状态
-    const updateTodo = () => {
-      localStorage.setItem('todos', JSON.stringify(todos.value))
-    }
-
-    // 删除待办事项
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1)
-    }
-
-    return {
-      todos,
-      newTodo,
-      addTodo,
-      updateTodo,
-      deleteTodo
-    }
+// 添加新待办事项
+const addTodo = () => {
+  if (newTodo.value.trim() !== '') {
+    todos.value.push({id: Date.now(), title: newTodo.value, completed: false})
+    newTodo.value = ''
   }
 }
+
+// 更新待办事项完成状态
+const updateTodo = () => {
+  localStorage.setItem('todos', JSON.stringify(todos.value))
+}
+
+// 删除待办事项
+const deleteTodo = (index) => {
+  todos.value.splice(index, 1)
+}
+
 </script>
 
 <style>
